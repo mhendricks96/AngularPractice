bcb4701311d5f2708e259f580b36abdce985077f
mhendricks96 <mhendricks96@yahoo.com>
2022-06-19 12:17:41 -0700

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+// requires selector and template
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
---
+++ src/app/media-item/media-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-media-item',
+  templateUrl: './media-item.component.html',
+  styleUrls: ['./media-item.component.scss']
+})
+export class MediaItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
